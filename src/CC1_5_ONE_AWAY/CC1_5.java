ab8cc164d05ce3d7640665270d1c968baba43c9e
TianyuWu1990 <wty773752792@gmail.com>
2017-06-15 17:13:13 -0400

    Q1.5

---
+++ src/CC1_5_ONE_AWAY/CC1_5.java
@@ -0,0 +1,11 @@
+/**
+ * Created by wutianyu on 6/15/17.
+ * There are three types of edits that can be performed on strings: insert a character, remove a character, or replace
+ * a character. Given two strings, write a function to check if they are one edit (or zero edits) away.
+ * EXAMPLE
+ * pale, ple -) true
+ * pales, pale - ) true
+ * pale, bale -) true
+ * pale, bae -) false
+ */
+package CC1_5_ONE_AWAY;
