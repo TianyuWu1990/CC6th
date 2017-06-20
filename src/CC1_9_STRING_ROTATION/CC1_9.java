b736dda33a2563c4669664e4bdf089d311fd7fe0
TianyuWu1990 <wty773752792@gmail.com>
2017-06-20 00:25:07 -0400

    CC1.9

---
+++ src/CC1_9_STRING_ROTATION/CC1_9.java
@@ -0,0 +1,7 @@
+/*
+ * Created by wutianyu on 6/19/17.
+ * String Rotation: Assume you have a method i 5 S u b s t r i n g which checks ifone word is a substring of another.
+ * Given two strings, 51 and 52, write code to check if 52 is a rotation of 51 using only one call to isSubstring
+ * (e.g., Uwaterbottleuis a rotation ofuerbottlewatU).
+ */
+package CC1_9_STRING_ROTATION;
